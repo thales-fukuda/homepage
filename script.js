ede94124e031b3d0806604c46c5531b4bee1d398
thales <thalesc@alunos.utfpr.edu.br>
2018-02-18 19:50:13 -0300

    updated

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var pic = document.getElementById('picture');
 var menu = document.getElementById('menu');
+var photo = document.getElementById('photo');
+var all_pics = ['waves', 'ancient', 'vernazza']
+
+var rand = Math.floor((Math.random() * all_pics.length));
+console.log(rand);
+photo.src = './imgs/' + all_pics[rand] + '.jpg';
 
 
