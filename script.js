5989f825e952346116715cda135e2d5894006fef
thales <thalesc@alunos.utfpr.edu.br>
2018-02-19 17:46:48 -0300

    images changed

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var pic = document.getElementById('picture');
 var menu = document.getElementById('menu');
 var photo = document.getElementById('photo');
-var all_pics = ['waves', 'ancient', 'vernazza']
+var all_pics = ['window.gif', 'flowers.gif', 'traing.gif', 'arena.gif', 'forest.gif'];
 
 var rand = Math.floor((Math.random() * all_pics.length));
 console.log(rand);
-photo.src = './imgs/' + all_pics[rand] + '.jpg';
+photo.src = './imgs/' + all_pics[rand];
 
 
