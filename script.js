e555008857fd471616cf855422555ad04a497395
thales <thalesc@alunos.utfpr.edu.br>
2018-02-19 19:10:58 -0300

    removed comments

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var pic = document.getElementById('picture');
 var menu = document.getElementById('menu');
 var photo = document.getElementById('photo');
 var all_pics = ['window.gif', 'flowers.gif', 'traing.gif', 'arena.gif', 'forest.gif'];
-
+// a
 var rand = Math.floor((Math.random() * all_pics.length));
 console.log(rand);
 photo.src = './imgs/' + all_pics[rand];
