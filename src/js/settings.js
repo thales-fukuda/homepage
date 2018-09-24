c3d3914f5dc831a26b9de0a184bf9c105ad57098
Thales <thales@hero99.com.br>
2018-09-24 19:24:30 -0300

    [ADD] - createNode function

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,19 +1,28 @@
 let settingOptions = document.getElementById('settings-options');
 
+const createNode = (i) => {
+    let wrapper = document.createElement('div');
+    let name = document.createElement('span');
+    let colorPicker = document.createElement('input');
+
+    colorPicker.type = 'color';
+    wrapper.classList.add('settings__options__option');
+    name.classList.add('settings__options__option__name');
+
+    wrapper.appendChild(name);
+    wrapper.appendChild(colorPicker);
+
+    name.innerText = i.name;
+    colorPicker.value = i.color;
+
+    return wrapper
+}
+
 const createLlist = () => {
     siteList.map(i => {
-        let wrapper = document.createElement('div');
-        let name = document.createElement('span');
-        let colorPicker = document.createElement('input');
-        colorPicker.type = 'color';
-        wrapper.classList.add('settings__options__option');
-        name.classList.add('settings__options__option__name');
-        wrapper.appendChild(name);
-        wrapper.appendChild(colorPicker);
-        name.innerText = i.name;
-        colorPicker.value = i.color;
-        console.log(wrapper);
-        settingOptions.appendChild(wrapper);
+        let node = createNode(i);
+        console.log(node);
+        settingOptions.appendChild(node);
     })
 }
 
