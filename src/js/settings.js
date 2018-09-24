0469054111af8f11e66334919d6f95aa5dd0351a
Thales <thales@hero99.com.br>
2018-09-24 19:29:46 -0300

    [CHANGE] - settings.js refactor

--- src/js/settings.js
+++ src/js/settings.js
@@ -5,13 +5,14 @@ const createNode = (i) => {
     let name = document.createElement('span');
     let colorPicker = document.createElement('input');
 
-    colorPicker.type = 'color';
+
     wrapper.classList.add('settings__options__option');
     name.classList.add('settings__options__option__name');
 
     wrapper.appendChild(name);
     wrapper.appendChild(colorPicker);
 
+    colorPicker.type = 'color';
     name.innerText = i.name;
     colorPicker.value = i.color;
 
@@ -21,7 +22,6 @@ const createNode = (i) => {
 const createLlist = () => {
     siteList.map(i => {
         let node = createNode(i);
-        console.log(node);
         settingOptions.appendChild(node);
     })
 }
